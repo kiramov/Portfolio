c3a27fefe8fda6d510c82ce69df4ed531167614d
azat <kiramov8521@gmail.com>
2021-10-18 17:29:52 +1200

    change menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector ('.menu__close');
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector ('.menu__close');
 
 hamburger.addEventListener('click', ()=> {
     menu.classList.add('active');
@@ -11,7 +11,7 @@ closeElem.addEventListener('click', ()=> {
 });
 
 const counters = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
+    lines = document.querySelectorAll('.skills__ratings-line span');
 
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
